259fb2217a4cdca8a5d4f9ca12ce6453fccc2445
Bình Thanh Phan <binh.phan@adnovum.vn>
2018-06-20 19:31:18 +0700

    Revert "Resolve Solid-Puzzle-3 The problem of this puzzle: what if another type of currency, namely Hungarian Forint is added, we might have to dive into the machine implementation and add modification as well. So the solution here is just try to resolve the problem not really apply SOLID"
    This reverts commit d4cc94b

--- Solid-Puzzle-3/src/com/greenduck/vendingmachine/machine/MonkeyFoodVendingMachine.java
+++ Solid-Puzzle-3/src/com/greenduck/vendingmachine/machine/MonkeyFoodVendingMachine.java
@@ -3,7 +3,7 @@ package com.greenduck.vendingmachine.machine;
 import com.greenduck.vendingmachine.foods.Food;
 import com.greenduck.vendingmachine.money.Banknote;
 import com.greenduck.vendingmachine.money.Currency;
-import com.greenduck.vendingmachine.money.CurrencyExchangeRate;
+import com.greenduck.vendingmachine.money.CurrencyExchangeRateConstants;
 
 /**
  * We hire a monkey coder to implement a sample vending machine for us. He gave
@@ -11,17 +11,73 @@ import com.greenduck.vendingmachine.money.CurrencyExchangeRate;
  */
 public class MonkeyFoodVendingMachine extends FoodVendingMachine {
 
-    private CurrencyExchangeRate currencyExchangeRate = new CurrencyExchangeRate();
-
     public MonkeyFoodVendingMachine(Currency currency) {
         super(currency);
     }
 
     @Override
     public void addBalance(Banknote note) {
-        Double currencyExchangeRateCur = currencyExchangeRate.getCurrencyExchangeRate(note.getCurrency(),
-                getCurrency());
-        this.balance += note.getAmount() * currencyExchangeRateCur;
+
+        /* Our machine currently use VND, so let's check what you put in */
+        if (Currency.VND.equals(getCurrency())) {
+            if (Currency.VND.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.VND_TO_VND;
+            }
+            if (Currency.CHF.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.CHF_TO_VND;
+            }
+            if (Currency.USD.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.USD_TO_VND;
+            }
+            if (Currency.EUR.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.EUR_TO_VND;
+            }
+        }
+
+        if (Currency.USD.equals(getCurrency())) {
+            if (Currency.USD.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.USD_TO_USD;
+            }
+            if (Currency.CHF.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.CHF_TO_USD;
+            }
+            if (Currency.VND.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.VND_TO_USD;
+            }
+            if (Currency.EUR.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.EUR_TO_USD;
+            }
+        }
+
+        if (Currency.CHF.equals(getCurrency())) {
+            if (Currency.CHF.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.CHF_TO_CHF;
+            }
+            if (Currency.VND.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.VND_TO_CHF;
+            }
+            if (Currency.USD.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.USD_TO_CHF;
+            }
+            if (Currency.EUR.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.EUR_TO_CHF;
+            }
+        }
+
+        if (Currency.EUR.equals(getCurrency())) {
+            if (Currency.EUR.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.EUR_TO_EUR;
+            }
+            if (Currency.CHF.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.CHF_TO_EUR;
+            }
+            if (Currency.USD.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.USD_TO_EUR;
+            }
+            if (Currency.VND.equals(note.getCurrency())) {
+                this.balance += note.getAmount() * CurrencyExchangeRateConstants.VND_TO_EUR;
+            }
+        }
     }
 
     @Override
@@ -31,10 +87,67 @@ public class MonkeyFoodVendingMachine extends FoodVendingMachine {
         double currentBalance = getBalance();
         Food selectedFood = foods.get(selectedIndex);
 
-        Double currencyExchangeRateCur = currencyExchangeRate
-                .getCurrencyExchangeRate(selectedFood.getPriceTag().getCurrency(), getCurrency());
+        /* check the machine's current currency to resolve appropriate balance */
 
-        this.balance -= selectedFood.getPriceTag().getPrice() * currencyExchangeRateCur;
+        if (Currency.VND.equals(getCurrency())) {
+            if (Currency.VND.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.VND_TO_VND;
+            }
+            if (Currency.CHF.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.CHF_TO_VND;
+            }
+            if (Currency.USD.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.USD_TO_VND;
+            }
+            if (Currency.EUR.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.EUR_TO_VND;
+            }
+        }
+
+        if (Currency.USD.equals(getCurrency())) {
+            if (Currency.USD.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.USD_TO_USD;
+            }
+            if (Currency.CHF.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.CHF_TO_USD;
+            }
+            if (Currency.VND.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.VND_TO_USD;
+            }
+            if (Currency.EUR.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.EUR_TO_USD;
+            }
+        }
+
+        if (Currency.EUR.equals(getCurrency())) {
+            if (Currency.EUR.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.EUR_TO_EUR;
+            }
+            if (Currency.CHF.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.CHF_TO_EUR;
+            }
+            if (Currency.USD.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.USD_TO_EUR;
+            }
+            if (Currency.VND.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.VND_TO_EUR;
+            }
+        }
+
+        if (Currency.CHF.equals(getCurrency())) {
+            if (Currency.CHF.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.CHF_TO_CHF;
+            }
+            if (Currency.VND.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.VND_TO_CHF;
+            }
+            if (Currency.USD.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.USD_TO_CHF;
+            }
+            if (Currency.EUR.equals(selectedFood.getPriceTag().getCurrency())) {
+                this.balance -= selectedFood.getPriceTag().getPrice() * CurrencyExchangeRateConstants.EUR_TO_CHF;
+            }
+        }
 
         if (balance < 0) {
             System.out.println("In Your Dream");
