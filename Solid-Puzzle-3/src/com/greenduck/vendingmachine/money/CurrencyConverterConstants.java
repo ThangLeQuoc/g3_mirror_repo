d4cc94be3fe440c534da7f5c79bb5c8f427f5b07
Bình Thanh Phan <binh.phan@adnovum.vn>
2018-06-20 19:23:18 +0700

    Resolve Solid-Puzzle-3 The problem of this puzzle: what if another type of currency, namely Hungarian Forint is added, we might have to dive into the machine implementation and add modification as well. So the solution here is just try to resolve the problem not really apply SOLID
    IMHO we cannot add "another type of currency" without change the old implement (we have to change the old implement to support convert to the new currency)
    I.g: we have 50 currencies which are implemented already (follow SOLID), after that we add one currency, I think we will have to update at least 51 classes.

---
+++ Solid-Puzzle-3/src/com/greenduck/vendingmachine/money/CurrencyConverterConstants.java
@@ -0,0 +1,23 @@
+package com.greenduck.vendingmachine.money;
+
+public class CurrencyConverterConstants {
+	public static final String CHF_TO_USD = "CHF_TO_USD";
+	public static final String EUR_TO_USD = "EUR_TO_USD";
+	public static final String VND_TO_USD = "VND_TO_USD";
+	public static final String USD_TO_USD = "USD_TO_USD";
+
+	public static final String CHF_TO_VND = "CHF_TO_VND";
+	public static final String EUR_TO_VND = "EUR_TO_VND";
+	public static final String VND_TO_VND = "VND_TO_VND";
+	public static final String USD_TO_VND = "USD_TO_VND";
+
+	public static final String CHF_TO_EUR = "CHF_TO_EUR";
+	public static final String EUR_TO_EUR = "EUR_TO_EUR";
+	public static final String VND_TO_EUR = "VND_TO_EUR";
+	public static final String USD_TO_EUR = "USD_TO_EUR";
+
+	public static final String CHF_TO_CHF = "CHF_TO_CHF";
+	public static final String EUR_TO_CHF = "EUR_TO_CHF";
+	public static final String VND_TO_CHF = "VND_TO_CHF";
+	public static final String USD_TO_CHF = "USD_TO_CHF";
+}
